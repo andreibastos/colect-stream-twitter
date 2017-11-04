e8fe8fdffa341aeb31c32f7dbffe5949d5fa53a4
Gustavo RPS <email+gitkraken.com@gustavorps.net>
2017-11-04 10:52:06 -0200

    improved test and fix data/twitter-statusues.json

--- settings.py
+++ settings.py
@@ -13,42 +13,42 @@ import dotenv
 __all__ = ['collector', 'endpoint', 'files', 'datasource']
 
 
-path_root = os.path.dirname(__file__)
-path_dotenv = os.path.join(path_root, '.env')
-path_settings_ini = os.path.join(path_root, 'settings.ini')
+PATH_ROOT = os.path.dirname(__file__)
+PATH_DOTENV = os.path.join(PATH_ROOT, '.env')
+PATH_SETTINGS_INI = os.path.join(PATH_ROOT, 'settings.ini')
 
-config = configparser.ConfigParser()
+__config = configparser.ConfigParser()
 
-if os.path.exists(path_settings_ini):
-  config.read(path_settings_ini)
+if os.path.exists(PATH_SETTINGS_INI):
+  __config.read(PATH_SETTINGS_INI)
 else:
-  if os.path.exists(path_dotenv):
-    dotenv.load_dotenv(path_dotenv)
+  if os.path.exists(PATH_DOTENV):
+    dotenv.load_dotenv(PATH_DOTENV)
 
   env = os.environ.get
   
-  config['collector'] = {}
-  config['collector']['categorize_namefield'] = env('CATEGORIZE_NAMEFIELD', '')
-  config['collector']['chat_id'] = env('TELEGRAM_CHAT_ID', '')
-  config['collector']['NUM_PER_INSERT'] = env('NUM_PER_INSERT', '10')
-  config['collector']['sendTelegram'] = env('TELEGRAM_BOT_ENABLED', 'False')
+  __config['collector'] = {}
+  __config['collector']['categorize_namefield'] = env('CATEGORIZE_NAMEFIELD', '')
+  __config['collector']['chat_id'] = env('TELEGRAM_CHAT_ID', '')
+  __config['collector']['NUM_PER_INSERT'] = env('NUM_PER_INSERT', '10')
+  __config['collector']['sendTelegram'] = env('TELEGRAM_BOT_ENABLED', 'False')
 
-  config['endpoint'] = {}
-  config['endpoint']['api_bot_telegram'] = env('TELEGRAM_BOT_API_URL', 'http://localhost:4000/telegram_bot')
-  config['endpoint']['api_categorize'] = env('CATEGORIZE_00_API_URL', 'http://localhost:4000/categorize_v1')
-  config['endpoint']['api_categorize2'] = env('CATEGORIZE_01_API_URL', 'http://localhost:4000/categorize_v2')
+  __config['endpoint'] = {}
+  __config['endpoint']['api_bot_telegram'] = env('TELEGRAM_BOT_API_URL', 'http://localhost:4000/telegram_bot')
+  __config['endpoint']['api_categorize'] = env('CATEGORIZE_00_API_URL', 'http://localhost:4000/categorize_v1')
+  __config['endpoint']['api_categorize2'] = env('CATEGORIZE_01_API_URL', 'http://localhost:4000/categorize_v2')
   # TODO: Move to datasource
-  config['endpoint']['api_database'] = env('DATASOURCE_API_URL', 'http://localhost:4000/categorize_v2')
+  __config['endpoint']['api_database'] = env('DATASOURCE_API_URL', 'http://localhost:4000/categorize_v2')
 
-  config['files'] = {}
-  config['files']['filename_log'] = env('TWITTER_LOG_FILE', os.path.join(path_root, 'twitter.log'))
-  config['files']['filename_keys'] = env('TWITTER_KEYS_FILE', os.path.join(path_root, 'keys.json'))
-  config['files']['filename_querys'] = env('TWITTER_QUERIES', os.path.join(path_root, 'queries.json'))
+  __config['files'] = {}
+  __config['files']['filename_log'] = env('TWITTER_LOG_FILE', os.path.join(PATH_ROOT, 'twitter.log'))
+  __config['files']['filename_keys'] = env('TWITTER_KEYS_FILE', os.path.join(PATH_ROOT, 'keys.json'))
+  __config['files']['filename_querys'] = env('TWITTER_QUERIES', os.path.join(PATH_ROOT, 'queries.json'))
 
-  config['datasource'] = {}
-  config['datasource']['es_uri'] = env('ES_URI', 'localhost:9200')
+  __config['datasource'] = {}
+  __config['datasource']['es_uri'] = env('ES_URI', 'localhost:9200')
 
-collector = config['collector']
-datasource = config['datasource']
-endpoint = config['endpoint']
-files = config['files']
+collector = __config['collector']
+datasource = __config['datasource']
+endpoint = __config['endpoint']
+files = __config['files']
