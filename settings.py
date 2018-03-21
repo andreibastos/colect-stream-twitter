25d99d124436636ce2cf02e7cd044a439c6ca0fd
Gustavo RPS <email+gitkraken.com@gustavorps.net>
2018-03-21 19:55:13 -0300

    backup

--- settings.py
+++ settings.py
@@ -10,45 +10,57 @@ from backports import configparser
 
 import dotenv
 
-__all__ = ['collector', 'endpoint', 'files', 'datasource']
+__all__ = ['twitter', 'logging']
 
 
-PATH_ROOT = os.path.dirname(__file__)
-PATH_DOTENV = os.path.join(PATH_ROOT, '.env')
-PATH_SETTINGS_INI = os.path.join(PATH_ROOT, 'settings.ini')
+_PATH_ROOT = os.path.dirname(__file__)
+_PATH_DOTENV = os.path.join(_PATH_ROOT, '.env')
+_PATH_SETTINGS_INI = os.path.join(_PATH_ROOT, 'settings.ini')
 
-__config = configparser.ConfigParser()
+_path_join_root = lambda path: os.path.join(_PATH_ROOT, path)
 
-if os.path.exists(PATH_SETTINGS_INI):
-  __config.read(PATH_SETTINGS_INI)
+_config = configparser.ConfigParser()
+
+if os.path.exists(_PATH_SETTINGS_INI):
+  _config.read(_PATH_SETTINGS_INI)
 else:
-  if os.path.exists(PATH_DOTENV):
-    dotenv.load_dotenv(PATH_DOTENV)
+  if os.path.exists(_PATH_DOTENV):
+    dotenv.load_dotenv(_PATH_DOTENV)
 
   env = os.environ.get
   
-  __config['collector'] = {}
-  __config['collector']['categorize_namefield'] = env('CATEGORIZE_NAMEFIELD', '')
-  __config['collector']['chat_id'] = env('TELEGRAM_CHAT_ID', '')
-  __config['collector']['NUM_PER_INSERT'] = env('NUM_PER_INSERT', '10')
-  __config['collector']['sendTelegram'] = env('TELEGRAM_BOT_ENABLED', 'False')
+  t = _config['twitter'] = {}
+  t['keys'] = {}
+  t['keys']['file'] = env('TWITTER_KEYS_FILE', _path_join_root('twitter-keys.json'))
+  t['keys']['rest'] = {}
+  t['keys']['rest']['uri'] = env('TWITTER_KEYS_REST_URI', None)
+  t['queries'] = {}
+  t['queries']['file'] = env('TWITTER_QUERIES_FILE', _path_join_root('twitter-queries.json'))
+  t['labeler'] = {}
+  t['labeler']['uri'] = env('TWITTER_LABELER_URI', None)
+  t['labeler']['property'] = env('TWITTER_LABELER_PROPERTY', 'keywords')
+  t['repository'] = {}
+  t['repository']['rest'] = {}
+  t['repository']['rest']['uri'] = env('TWITTER_REPOSITORY_REST_URI', None)
+  t['repository']['rest']['bulk_site'] = env('TWITTER_REPOSITORY_REST_BULK_SIZE', 10)
+  t['repository']['es'] = {}
+  t['repository']['es']['uri'] = env('TWITTER_REPOSITORY_ES_URI', None)
+  t['repository']['es']['bulk_site'] = env('TWITTER_REPOSITORY_ES_BULK_SIZE', 50)
 
-  __config['endpoint'] = {}
-  __config['endpoint']['api_bot_telegram'] = env('TELEGRAM_BOT_API_URL', 'http://localhost:4000/telegram_bot')
-  __config['endpoint']['api_categorize'] = env('CATEGORIZE_00_API_URL', 'http://localhost:4000/categorize_v1')
-  __config['endpoint']['api_categorize2'] = env('CATEGORIZE_01_API_URL', 'http://localhost:4000/categorize_v2')
-  # TODO: Move to datasource
-  __config['endpoint']['api_database'] = env('DATASOURCE_API_URL', 'http://localhost:4000/categorize_v2')
+  l = _config['logging'] = {}
+  l['handlers'] = {}
+  # REF: https://github.com/sashgorokhov/python-telegram-handler
+  l['handlers']['telegram'] = {}
+  l['handlers']['telegram']['class'] = env('LOGGING_HANDLERS_TELEGRAM_CLASS', None)
+  l['handlers']['telegram']['token'] = env('LOGGING_HANDLERS_TELEGRAM_TOKEN', None)
+  l['handlers']['telegram']['chat_id'] = env('LOGGING_HANDLERS_TELEGRAM_CHAT_ID', None)
+  # REF: https://docs.python.org/3/library/logging.config.html#dictionary-schema-details
+  l['handlers']['file'] = {}
+  l['handlers']['file']['class'] = env('LOGGING_HANDLERS_FILE_CLASS', 'logging.handlers.RotatingFileHandler')
+  l['handlers']['file']['formatter'] = env('LOGGING_HANDLERS_FILE_FORMATTER', 'precise')
+  l['handlers']['file']['filename'] = env('LOGGING_HANDLERS_FILE_FILENAME', 'logging.log')
+  l['handlers']['file']['maxBytes'] = env('LOGGING_HANDLERS_FILE_MAX_BYTES', 1024)
+  l['handlers']['file']['backupCount'] = env('LOGGING_HANDLERS_FILE_BACKUP_COUNT', 2)
 
-  __config['files'] = {}
-  __config['files']['filename_log'] = env('TWITTER_LOG_FILE', os.path.join(PATH_ROOT, 'twitter.log'))
-  __config['files']['filename_keys'] = env('TWITTER_KEYS_FILE', os.path.join(PATH_ROOT, 'keys.json'))
-  __config['files']['filename_querys'] = env('TWITTER_QUERIES', os.path.join(PATH_ROOT, 'queries.json'))
-
-  __config['datasource'] = {}
-  __config['datasource']['es_uri'] = env('ES_URI', 'localhost:9200')
-
-collector = __config['collector']
-datasource = __config['datasource']
-endpoint = __config['endpoint']
-files = __config['files']
+twitter = _config['twitter']
+logging = _config['logging']
